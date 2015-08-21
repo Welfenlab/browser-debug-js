6568891f161dc44b7f2c350212f66dc78e57f04f
Maximilian Klein <mklein@welfenlab.de>
2015-08-21 15:35:10 +0200

    (feature) Run code in sandboxed iframe.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 // Dynamically evaluate JavaScript-as-string in the browser
+var counter = 0;
 function injectCode(js){
-  var e = document.createElement('script');
-  e.type = 'text/javascript';
-  e.src  = 'data:text/javascript;charset=utf-8,'+escape(js);
-  document.body.appendChild(e);
+  var iframe = document.createElement("iframe");
+  iframe.setAttribute("sandbox","allow-scripts allow-same-origin")
+  iframe.style.display = "none";
+  document.body.appendChild(iframe);
+  frames[frames.length - 1].document.write(
+    "<script>"+js+"\n//@ sourceURL=debug"+counter+".js<\/script>"
+  );
+  
+  setTimeout(function(){
+    frames[frames.length - 1].document.clear();
+    document.body.removeChild(iframe);
+  }, 1000);
 }
 
 module.exports = {
